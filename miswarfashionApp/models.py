b062e86df0a07ba2cf02eb0c960ded40f295ca9a
bita639 <01625326156dj@gmail.com>
2021-09-24 02:47:24 +0600

    database model created

--- miswarfashionApp/models.py
+++ miswarfashionApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+
+class Product(models.Model):
+    image = models.ImageField(upload_to="product-image", default='packageImage/blank.png', blank=True, null=True)
+    name = models.CharField(max_length=40)
+    Category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='category')
+    size = models.CharField(max_length=200, blank = True)
+    description = models.TextField(blank = True)
+
